63c555b7b4fe84979816d7c1686610a064c7acb3
Abideen Bello <belloabideen@gmail.com>
2025-03-15 07:49:11 +0100

    output cleaned-up

--- output.tf
+++ output.tf
@@ -1,48 +1,29 @@
-output "EC2-instance-1_arn" {
-  description = "my first ecc2 instance id"
-  value       = aws_instance.Terra_deployed_1.arn
+output "EC2-primary_instance_arn" {
+  description = "ec2 instance arn"
+  value       = aws_instance.primary_instance.arn
 
 }
-output "EC2-instance-1_id" {
-  description = "my first ecc2 instance id"
-  value       = aws_instance.Terra_deployed_1.id
-}
-
-
-output "EC2-instance-1_az" {
-  description = "my first ecc2 instance id"
-  value       = aws_instance.Terra_deployed_1.availability_zone
+output "primary_instance_id" {
+  description = "ec2 instance id"
+  value       = aws_instance.primary_instance.id
 
 }
 
-output "EC2-instance-1_volume" {
-  description = "my first ecc2 instance id"
-  value       = aws_instance.Terra_deployed_1.public_ip
-}
-
-output "First_volume_arn" {
-  value = aws_ebs_volume.my-ec2-volume.arn
+output "primary_instance_az" {
+  description = "ec2 instance az"
+  value       = aws_instance.primary_instance.availability_zone
 
 }
-
-output "First_volume_az" {
-  value = aws_ebs_volume.my-ec2-volume.availability_zone
-
-}
-output "First_volume_id" {
-  value = aws_ebs_volume.my-ec2-volume.id
+output "primary_volume_id" {
+  description = "Attached EBS volune id"
+  value       = aws_ebs_volume.primary_volume.id
 
 }
 
-
-output "Second_volume_id" {
-  value = aws_volume_attachment.ec2-2nd-volume.id
-}
-
-output "first-efs_id" {
-  value = aws_efs_file_system.first-efs.id
+output "primary_volume_arn" {
+  value = aws_ebs_volume.primary_volume.arn
 }
 
 output "my_ami_image" {
-  value = null_resource.MY-AMI.id
+  value = null_resource.create_ami.id
 }
